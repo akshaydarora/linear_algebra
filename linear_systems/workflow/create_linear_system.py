ac5524ec6e8ec584e439b66e61f6fa037caa8438
Akshay <akshayarora1909@gmail.com>
2023-02-25 00:42:58 -0600

    Update LinearSystem

--- linear_systems/workflow/create_linear_system.py
+++ linear_systems/workflow/create_linear_system.py
@@ -1,70 +1,40 @@
-from services.decomposers.qr_decomposition import QRDecomposition
-from services.decomposers.single_value_decomposition import SVD
-from services.factorizers.cholesky_factorizer import CholeskyFactorizer
-from services.save_load_ls.save_load_linear_system import SaveLoadLS
+import json
+import os
+from services.initializers.constraint_matrix_initializer import Matrix
+from services.initializers.right_hand_side_initializer import RHS
+from linear_system import LinearSystem
 
-class CreateLinearSystem(object):
+if __name__=="__main__":
+    dir_name=os.path.dirname(os.path.realpath(__file__)) 
+    data_dir="files/input/"
+    ######## Initialize Params #############
+    with open(os.path.join(dir_name,"config/config.json"),"r",encoding='utf-8') as f:
+        config=json.load(f)
 
-    def __init__(self,a,config):
-        self.a=a
-        self.config=config
-    
-    # Initialize Decomposition
-    def decompose(self,method_type,method_name,*args,**kwargs):
-        decomposition_status={}
-        file_dir=self.config[method_type][method_name]["file_dir"]
-        file_name=self.config[method_type][method_name]["file_name"]
-        if method_name.upper()=="QR":
-            qr_method="gram_schmidt"
-            qr=QRDecomposition(self.a,qr_method)
-            Q,R=qr.getQRDecomposeResult()
-            QR_result={}
-            QR_result["Q"]=Q
-            QR_result["R"]=R
-            sls=SaveLoadLS(file_obj=QR_result,file_name=file_name,file_dir=file_dir)
-            sls.save_file()
-            decomposition_status["status"]="success"
-            decomposition_status["message"]="successully saved the {} : decomposed file {} in {} location".format(method_name,
-                                                                                                                  file_name,
-                                                                                                                  file_dir)
+    with open(data_dir+"data_A.json","r",encoding='utf-8') as f:
+        input_data=json.load(f)
+
+    A=input_data["A"]    
+    B=[1,2,3]
+
+    # Initialize the matrix A
+    a=Matrix(A)
+
+    b=RHS(B)
+    b.checkRHS()
+
+    ############ Create Linear System #####################
+
+    # Initialize the Methods
+    method_type="decomposition"
+    method_name="svd"
+    cls=LinearSystem(a,config)
+    if method_type=="decomposition":
+        decompose_status=cls.decompose(method_type,method_name)
+        print(decompose_status)
+
+    elif method_type=="factorization":
+        factorize_status=cls.factorize(method_type,method_name)
+        print(factorize_status)
 
 
-        elif method_name.upper()=="SVD":
-            svd=SVD(self.a)
-            U,sigma,VT=svd.getSVDResult()
-            SVD_result={}
-            SVD_result["U"]=U
-            SVD_result["sigma"]=sigma
-            SVD_result["VT"]=VT
-            sls=SaveLoadLS(file_obj=SVD_result,file_name=file_name,file_dir=file_dir)
-            sls.save_file()
-            decomposition_status["status"]="success"
-            decomposition_status["message"]="successully saved the {} : decomposed file {} in {} location".format(method_name,
-                                                                                                                  file_name,
-                                                                                                                  file_dir)
-        else:
-            raise Exception("please choose decompose method in ('QR','SVD')")
-
-        return decomposition_status    
-    
-    # Initialize Factorization
-    def factorize(self,method_type,method_name,*args,**kwargs):
-        factorization_status={}
-        file_dir=self.config[method_type][method_name]["file_dir"]
-        file_name=self.config[method_type][method_name]["file_name"]
-        if method_name.upper()=="CHOLESKY":
-            L_result={}
-            chs=CholeskyFactorizer(self.a)
-            L=chs.getCHFResult()
-            L_result["L"]=L
-            sls=SaveLoadLS(file_obj=L_result,file_name=file_name,file_dir=file_dir)
-            sls.save_file()
-            factorization_status["status"]="success"
-            factorization_status["message"]="successully saved the {} : factorized file {} in {} location".format(method_name,
-                                                                                                                  file_name,
-                                                                                                                  file_dir)
-        else:
-            raise Exception("please choose factorize method in ('CHS')") 
-        
-        return factorization_status
-    
